d8d8455385bf9c135bc61c5a5f74e1cd922c8e0f
Helen205 <helenuzncyr@gmail.com>
2023-12-30 14:26:32 +0300

    düzenleme

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import mysql.connector
-
-from flask import Flask, render_template
-
-app = Flask(__name__, static_folder='static', static_url_path='/static')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
     
 # MySQL bağlantısı
 conn = mysql.connector.connect(
